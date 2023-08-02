b8418ae9357020711bb7885d16c4d877c5c1ec6e
Rithvik-padma <rithvik.padma@gmail.com>
2023-08-02 16:43:53 +0530

    Complete the hero section

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -28,12 +28,22 @@ const Hero = () => {
             <img 
                 src="/assets/intro-cube-1.png" 
                 alt="Intro Cube 1"
-                className='absolute left-[15%] w-28 smlap:left-[10%] smlap:w-24 mob:!left-[7%] mob:top-[10%] mob:!w-16' 
+                className='absolute left-[15%] w-28 smlap:left-[10%] smlap:w-24 smlap:top-[10%] mob:!left-[7%] mob:top-[10%] mob:!w-16' 
             />
             <img 
                 src="/assets/intro-stairs-1.png" 
                 alt="Intro Stairs"
-                className='z-5 absolute right-[12%] w-52 smlap:right-[8%] smlap:w-32 mob:!right-[7%] mob:!top-[10%]' 
+                className='z-5 absolute right-[12%] w-52 smlap:right-[8%] smlap:w-48 smlap:top-[10%] mob:!right-[7%] mob:!w-32' 
+            />
+            <img 
+                src="/assets/intro-cubes-1.png" 
+                alt="Intro Cubes 1"
+                className='absolute left-[15%] bottom-12 w-52 smlap:left-[10%] smlap:w-32 smlap:bottom-[10%] mob:!left-[7%]' 
+            />
+            <img 
+                src="/assets/intro-pillar-1.png" 
+                alt="Intro Pillar 1"
+                className='absolute right-[20%] bottom-[-11%] w-72 smlap:right-[9%] smlap:bottom-[-5%] smlap:w-72 mob:!right-[-15%] mob:!bottom-[8%] mob:!w-48' 
             />
         </div>
     </section>
