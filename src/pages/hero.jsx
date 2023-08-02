bbf5b203fcb6b7c7792ccb6b12f1d0b6f502c0cc
Rithvik-padma <rithvik.padma@gmail.com>
2023-08-02 16:25:52 +0530

    Added cube and stairs

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -10,13 +10,13 @@ const Hero = () => {
             alt="Vector blast"
             className='w-[100dvw] h-[100dvh] object-cover absolute top-0 left-0' 
         />
-        <div className='text-center text-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-46%] text-[150px] mob:text-[75px] leading-[1] font-Agrandir py-10'>
-            <div className='relative -z-10'>CASH</div>
-            <div className='z-20'>APP</div>
+        <div className='text-center text-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-46%] text-[150px] smlap:text-[110px] mob:!text-[80px] leading-[1] font-Agrandir py-10'>
+            <div className='relative z-10'>CASH</div>
+            <div className='relative z-20'>APP</div>
             <img 
                 src="/assets/intro-phone-1.png" 
                 alt="Intro phone"
-                className='h-full w-auto absolute z-[-10] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]' 
+                className='h-full w-auto absolute z-[15] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]' 
             />
         </div>
         <img 
@@ -24,6 +24,18 @@ const Hero = () => {
             alt="Scroll down indicator"
             className='w-7 h-9 mob:w-5 mob:h-7 absolute bottom-0 left-[50%] translate-x-[-50%] mb-6' 
         />
+        <div>
+            <img 
+                src="/assets/intro-cube-1.png" 
+                alt="Intro Cube 1"
+                className='absolute left-[15%] w-28 smlap:left-[10%] smlap:w-24 mob:!left-[7%] mob:top-[10%] mob:!w-16' 
+            />
+            <img 
+                src="/assets/intro-stairs-1.png" 
+                alt="Intro Stairs"
+                className='z-5 absolute right-[12%] w-52 smlap:right-[8%] smlap:w-32 mob:!right-[7%] mob:!top-[10%]' 
+            />
+        </div>
     </section>
   )
 }
