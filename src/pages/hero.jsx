a308989171b9ff4264b4fbc411613e7b8f38e60d
Rithvik-padma <rithvik.padma@gmail.com>
2023-08-02 16:05:02 +0530

    Hero Cash app section completedf

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -10,9 +10,14 @@ const Hero = () => {
             alt="Vector blast"
             className='w-[100dvw] h-[100dvh] object-cover absolute top-0 left-0' 
         />
-        <div className=''>
-            <div>CASH</div>
-            <div>APP</div>
+        <div className='text-center text-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-46%] text-[150px] mob:text-[75px] leading-[1] font-Agrandir py-10'>
+            <div className='relative -z-10'>CASH</div>
+            <div className='z-20'>APP</div>
+            <img 
+                src="/assets/intro-phone-1.png" 
+                alt="Intro phone"
+                className='h-full w-auto object-cover absolute z-[-10] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]' 
+            />
         </div>
     </section>
   )
