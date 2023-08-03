66b8057fe6bf7b9692fab13b416097caa5586959
Rithvik-padma <rithvik.padma@gmail.com>
2023-08-03 14:25:22 +0530

    Add the pillars in the payments section page for laptop screen

--- src/pages/payments.jsx
+++ src/pages/payments.jsx
@@ -4,7 +4,7 @@ const Payments = () => {
   return (
     <section className='h-[100dvh] bg-[#F8F6F6] snap-start relative'>
       <div className='absolute flex items-center smlap:flex-col-reverse revsmlap:w-[650px] top-[50%] right-[50%] -translate-y-[50%] smlap:translate-x-[50%] smlap:gap-10 mob:w-[300px] z-10'>
-        <div className='relative z-10 revsmlap:left-52'>
+        <div className='relative z-10 revsmlap:left-52 revsmlap:bottom-10'>
           <h1 className='text-2xl mob:text-xl font-Agrandir text-[#00D54B]'>
             Payments
           </h1>
@@ -19,6 +19,21 @@ const Payments = () => {
           alt="Payments Phone 1" 
         />
       </div>
+      <img 
+        src="/assets/payments-column-1.png"
+        alt="Payments column 1"
+        className='absolute bottom-0 w-[180px]' 
+      />
+      <img 
+        src="/assets/payments-column-2.png"
+        alt="Payments column 2"
+        className='absolute bottom-0 h-[410px] right-[20%] z-10' 
+      />
+      <img 
+        src="/assets/payments-column-3.png"
+        alt="Payments column 3"
+        className='absolute bottom-[8%] h-[445px] right-0' 
+      />
     </section>
   )
 }
