97b60cb57e703d89b9d20a2b1ce167f5aeaa4e22
Rithvik-padma <rithvik.padma@gmail.com>
2023-08-06 23:42:46 +0530

    Add few image additions

--- src/pages/banking.jsx
+++ src/pages/banking.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Banking = () => {
   return (
     <section className='h-[100dvh] bg-[#00D54B] snap-start relative'>
-      <div className='absolute flex items-center smlap:flex-col-reverse revsmlap:w-[370px] top-[50%] right-[50%] -translate-y-[50%] -translate-x-[25%] smlap:w-[300px] smlap:translate-x-[50%] smlap:gap-10 mob:!w-[200px] z-20'>
-        <div className='relative z-10 revsmlap:left-10 revsmlap:bottom-10'>
+      <div className='absolute flex items-center smlap:flex-col-reverse revsmlap:w-[370px] top-[50%] right-[50%] -translate-y-[50%] -translate-x-[25%] smlap:w-[300px] smlap:translate-x-[50%] smlap:gap-10 mob:!w-[200px] z-50'>
+        <div className='relative z-10 revsmlap:left-10'>
           <h1 className='text-2xl mob:text-xl font-Agrandir text-white'>
             Banking
           </h1>
@@ -18,6 +18,21 @@ const Banking = () => {
           alt="Banking phone" 
         />
       </div>
+      <img
+        src="/assets/banking-hole.png" 
+        alt="Banking hole top"
+        className='absolute top-0 left-0' 
+      />
+      <img
+        src="/assets/banking-column.png" 
+        alt="Banking column" 
+        className='absolute top-[14%] left-[5%] z-10'
+      />
+      <img
+        src="/assets/banking-stairs-2.png" 
+        alt="Banking Stairs 2"
+        className='absolute bottom-[13%] left-[4%] z-10' 
+      />
     </section>
   )
 }
