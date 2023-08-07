42bafa2e625e5f5e41991c9ad137252fd6754b82
Rithvik-padma <rithvik.padma@gmail.com>
2023-08-07 15:48:38 +0530

    Add the boost section page in large screen view

--- src/pages/boost.jsx
+++ src/pages/boost.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Boost = () => {
   return (
     <section className='h-[100dvh] bg-black snap-start relative'>
-      <div className='absolute flex items-center gap-10 smlap:flex-col-reverse revsmlap:w-[24vw] top-[54%] right-[50%] -translate-y-[50%] -translate-x-[22%] smlap:w-[300px] smlap:translate-x-[50%] smlap:gap-8 mob:!w-[200px] z-50'>
+      <div className='absolute flex items-center gap-10 smlap:flex-col-reverse mdlap:scale-90 revsmlap:w-[24vw] top-[54%] right-[50%] -translate-y-[50%] -translate-x-[22%] smlap:w-[300px] smlap:translate-x-[50%] smlap:gap-8 mob:!w-[200px] z-50'>
         <div className='relative z-10 revsmlap:bottom-20'>
           <h1 className='text-3xl mob:text-xl font-Agrandir text-[#00D54B]'>
             Cash Card & Boost
@@ -27,6 +27,31 @@ const Boost = () => {
         src="/assets/boost-stairs-1.png" 
         alt="Boost Stairs 1" 
         className='absolute bottom-0 right-0 w-[60%]'
+      />
+      <img
+        src="/assets/boost-shoe.png" 
+        alt="Boost Shoe"
+        className='absolute bottom-[34.7%] right-[1.4%] w-[6.5%]'
+      />
+      <img
+        src="/assets/boost-coffee.png" 
+        alt="Boost Coffee" 
+        className='absolute bottom-[16%] right-[20%] w-[5%]'
+      />
+      <img
+        src="/assets/boost-burger.png" 
+        alt="Boost Burger" 
+        className='absolute bottom-[35%] right-[26.5%] w-[4.8%]'
+      />
+      <img
+        src="/assets/boost-card.png" 
+        alt="Boost Card" 
+        className='absolute bottom-[53%] right-[20%] w-[5%]'
+      />
+      <img
+        src="/assets/boost-hand.png" 
+        alt="Boost Hand" 
+        className='absolute top-[14.5%] right-[13%] w-[4.7%]'
       />
     </section>
   )
