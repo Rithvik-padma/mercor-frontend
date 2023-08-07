96bfde18b4b26929d3bf41cabfd891340d79a4a3
Rithvik-padma <rithvik.padma@gmail.com>
2023-08-07 15:57:56 +0530

    Update the boost section page in small screen view

--- src/pages/boost.jsx
+++ src/pages/boost.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Boost = () => {
   return (
     <section className='h-[100dvh] bg-black snap-start relative'>
-      <div className='absolute flex items-center gap-10 smlap:flex-col-reverse mdlap:scale-90 revsmlap:w-[24vw] top-[54%] right-[50%] -translate-y-[50%] -translate-x-[22%] smlap:w-[300px] smlap:translate-x-[50%] smlap:gap-8 mob:!w-[200px] z-50'>
+      <div className='absolute flex items-center gap-10 smlap:flex-col-reverse mdlap:scale-90 revsmlap:w-[24vw] top-[50%] right-[50%] -translate-y-[50%] -translate-x-[22%] mob:top-[46%] smlap:w-[300px] smlap:translate-x-[50%] smlap:gap-8 mob:!w-[200px] z-50'>
         <div className='relative z-10 revsmlap:bottom-20'>
           <h1 className='text-3xl mob:text-xl font-Agrandir text-[#00D54B]'>
             Cash Card & Boost
           </h1>
-          <p className='revsmlap:w-[350px] mob:w-[86dvw] text-white font-Mulish mob:text-sm'>
+          <p className='revsmlap:w-[350px] mob:w-[92.4vw] text-white font-Mulish mob:text-sm'>
             The Cash Card is a free, customizable debit card that lets you pay online and in stores. It’s the only way to get Boosts—instant discounts that work at places where you want to spend.
           </p>
         </div>
@@ -31,27 +31,27 @@ const Boost = () => {
       <img
         src="/assets/boost-shoe.png" 
         alt="Boost Shoe"
-        className='absolute bottom-[34.7%] right-[1.4%] w-[6.5%]'
+        className='absolute bottom-[34.7%] right-[1.4%] w-[6.5%] smlap:hidden'
       />
       <img
         src="/assets/boost-coffee.png" 
         alt="Boost Coffee" 
-        className='absolute bottom-[16%] right-[20%] w-[5%]'
+        className='absolute bottom-[16%] right-[20%] w-[5%] smlap:hidden'
       />
       <img
         src="/assets/boost-burger.png" 
         alt="Boost Burger" 
-        className='absolute bottom-[35%] right-[26.5%] w-[4.8%]'
+        className='absolute bottom-[35%] right-[26.5%] w-[4.8%] smlap:hidden'
       />
       <img
         src="/assets/boost-card.png" 
         alt="Boost Card" 
-        className='absolute bottom-[53%] right-[20%] w-[5%]'
+        className='absolute bottom-[53%] right-[20%] w-[5%] smlap:hidden'
       />
       <img
         src="/assets/boost-hand.png" 
         alt="Boost Hand" 
-        className='absolute top-[14.5%] right-[13%] w-[4.7%]'
+        className='absolute top-[14.5%] right-[13%] w-[4.7%] smlap:hidden'
       />
     </section>
   )
