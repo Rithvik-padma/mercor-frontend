77d8f811a6f30388de20dab15d856e6e5e26bcdc
Rithvik-padma <rithvik.padma@gmail.com>
2023-08-07 23:47:52 +0530

    Add changes for investing section page for mobile screen view

--- src/pages/investing.jsx
+++ src/pages/investing.jsx
@@ -3,37 +3,37 @@ import React from 'react'
 const Investing = () => {
   return (
     <section className='h-[100dvh] bg-[#00D54B] snap-start relative'>
-      <div className='absolute w-[100%] top-[50%] left-[50%] -translate-x-[50%] revsmlap:-translate-y-[60%] smlap:top-[3%] flex flex-col gap-16 smlap:gap-12 items-center z-50'>
+      <div className='absolute w-[100%] mob:!h-[85%] sm:top-[50%] left-[50%] -translate-x-[50%] revsmlap:-translate-y-[60%] smlap:top-[3%] flex flex-col gap-16 smlap:gap-12 mob:!gap-8 items-center z-50 mob:!justify-center'>
         <h1 className='text-3xl mob:text-xl font-Agrandir text-white'>
           Investing
         </h1>
-        <div className='flex flex-row gap-20 smlap:gap-10 items-center'>
-          <div className='flex flex-row gap-8 justify-center items-center smlap:flex-row-reverse'>
+        <div className='w-[86dvw] flex flex-row gap-20 mob:!h-full smlap:gap-10 items-center mob:!flex-col mob:!h-full mob:!justify-around'>
+          <div className='flex flex-row gap-8 justify-center items-center smlap:flex-row-reverse mob:!flex-col-reverse'>
             <div className='revsmlap:-translate-y-[50%]'>
-              <h1 className='text-xl text-black font-Agrandir mob:text-lg'>
+              <h1 className='text-xl text-black font-Agrandir mob:!text-[16px]'>
                 Stocks
               </h1>
-              <p className='revsmlap:w-[350px] mob:w-[86dvw] text-black font-Mulish mob:text-sm smlap:w-[300px]'>
+              <p className='revsmlap:w-[350px] mob:!w-[86dvw] text-black font-Mulish mob:text-sm smlap:w-[300px]'>
                 Whether you’re an expert or just getting started, Cash App is the fastest and most accessible way to invest in stocks. Start now with as little as $1.
               </p>
             </div>
             <img 
               src="/assets/investing-stocks.png" 
               alt="Investing Stocks" 
-              className='block smlap:w-[100px]'
+              className='block smlap:w-[100px] mob:!w-[50px]'
             />
           </div>
-          <div className='flex flex-row gap-8 justify-center items-center'>
+          <div className='w-[86dvw] flex flex-row gap-8 justify-center items-center mob:!flex-col'>
             <img 
               src="/assets/investing-bitcoin.png" 
               alt="Investing Bitcoin" 
-              className='block smlap:w-[100px]'
+              className='block smlap:w-[100px] mob:!w-[50px]'
             />
             <div className='revsmlap:-translate-y-[50%]'>
-              <h1 className='text-xl text-black font-Agrandir mob:text-lg'>
+              <h1 className='text-xl text-black font-Agrandir mob:!text-[16px]'>
                 Bitcoin
               </h1>
-              <p className='revsmlap:w-[350px] mob:w-[86dvw] text-black font-Mulish mob:text-sm smlap:w-[300px]'>
+              <p className='revsmlap:w-[350px] mob:!w-[86dvw] text-black font-Mulish mob:text-sm smlap:w-[300px]'>
                 Cash App is the fastest way to convert dollars to bitcoin. From your home screen, six taps are all it takes to stack sats, buy an entire bitcoin, or just see what it’s all about.
               </p>
             </div>
@@ -48,17 +48,17 @@ const Investing = () => {
       <img 
         src="/assets/investing-floor.png" 
         alt="Investing Floor" 
-        className='absolute bottom-0 w-full'
+        className='absolute bottom-0 w-full mob:h-[50%]'
       />
       <img 
         src="/assets/investing-graph-1.png" 
         alt="Investing Graph 1" 
-        className='absolute left-0 bottom-0'
+        className='absolute left-0 bottom-0 mob:hidden'
       />
       <img 
         src="/assets/investing-graph-2.png" 
         alt="Investing Graph 2" 
-        className='absolute right-0 bottom-[35%]'
+        className='absolute right-0 bottom-[35%] mob:hidden'
       />
     </section>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'mob': {'max': '750px'},
+      'mob': {'max': '920px'},
       'smlap': {'max': '1200px'},
       'revsmlap': '1200px',
       'mdlap': {'max': '1350px'}
