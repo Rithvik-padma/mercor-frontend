cf9f3796dff53a76a833df07694eaf48ea776a05
Rithvik-padma <rithvik.padma@gmail.com>
2023-08-07 23:15:00 +0530

    Change to the investing page for small laptop screen view

--- src/pages/investing.jsx
+++ src/pages/investing.jsx
@@ -3,37 +3,37 @@ import React from 'react'
 const Investing = () => {
   return (
     <section className='h-[100dvh] bg-[#00D54B] snap-start relative'>
-      <div className='absolute w-[100%] top-[50%] left-[50%] -translate-x-[50%] -translate-y-[60%] flex flex-col gap-16 items-center z-50'>
+      <div className='absolute w-[100%] top-[50%] left-[50%] -translate-x-[50%] revsmlap:-translate-y-[60%] smlap:top-[3%] flex flex-col gap-16 smlap:gap-12 items-center z-50'>
         <h1 className='text-3xl mob:text-xl font-Agrandir text-white'>
           Investing
         </h1>
-        <div className='flex flex-row gap-20 items-center mob:flex-col'>
-          <div className='flex flex-row gap-8 justify-center items-center'>
-            <div className='-translate-y-[50%]'>
+        <div className='flex flex-row gap-20 smlap:gap-10 items-center'>
+          <div className='flex flex-row gap-8 justify-center items-center smlap:flex-row-reverse'>
+            <div className='revsmlap:-translate-y-[50%]'>
               <h1 className='text-xl text-black font-Agrandir mob:text-lg'>
                 Stocks
               </h1>
-              <p className='revsmlap:w-[350px] mob:w-[86dvw] text-black font-Mulish mob:text-sm'>
+              <p className='revsmlap:w-[350px] mob:w-[86dvw] text-black font-Mulish mob:text-sm smlap:w-[300px]'>
                 Whether you’re an expert or just getting started, Cash App is the fastest and most accessible way to invest in stocks. Start now with as little as $1.
               </p>
             </div>
             <img 
               src="/assets/investing-stocks.png" 
               alt="Investing Stocks" 
-              className='block'
+              className='block smlap:w-[100px]'
             />
           </div>
-          <div className='flex flex-row gap-8 items-center mob:flex-col'>
+          <div className='flex flex-row gap-8 justify-center items-center'>
             <img 
               src="/assets/investing-bitcoin.png" 
               alt="Investing Bitcoin" 
-              className='block'
+              className='block smlap:w-[100px]'
             />
-            <div className='-translate-y-[50%]'>
+            <div className='revsmlap:-translate-y-[50%]'>
               <h1 className='text-xl text-black font-Agrandir mob:text-lg'>
                 Bitcoin
               </h1>
-              <p className='revsmlap:w-[350px] mob:w-[86dvw] text-black font-Mulish mob:text-sm'>
+              <p className='revsmlap:w-[350px] mob:w-[86dvw] text-black font-Mulish mob:text-sm smlap:w-[300px]'>
                 Cash App is the fastest way to convert dollars to bitcoin. From your home screen, six taps are all it takes to stack sats, buy an entire bitcoin, or just see what it’s all about.
               </p>
             </div>
